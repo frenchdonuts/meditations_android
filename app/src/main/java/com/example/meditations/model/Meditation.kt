5fa5d012c8639d2f655f387f2d555968fda2abbc
frenchdonuts <jonathantan1991@gmail.com>
2020-04-28 18:53:26 -0700

    Create static mapper fn on Meditation from com.example.meditations.Meditation;

--- app/src/main/java/com/example/meditations/model/Meditation.kt
+++ app/src/main/java/com/example/meditations/model/Meditation.kt
@@ -1,4 +1,14 @@
 package com.example.meditations.model
 
+
 data class Meditation(val id: String,
-                      val text: String)
+                      val text: String) {
+    companion object {
+        fun from(meditation: com.example.meditations.Meditation): Meditation {
+            return Meditation(
+                meditation.id.toString(),
+                meditation.text
+            )
+        }
+    }
+}
