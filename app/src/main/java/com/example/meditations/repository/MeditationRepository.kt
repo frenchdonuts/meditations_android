3146347e530b192b48828812ef190b52d5dc59ad
frenchdonuts <jonathantan1991@gmail.com>
2020-05-06 13:40:00 -0700

    Have MeditationRepository.create() return Completable instead of Single<Meditation>.

--- app/src/main/java/com/example/meditations/repository/MeditationRepository.kt
+++ app/src/main/java/com/example/meditations/repository/MeditationRepository.kt
@@ -1,8 +1,10 @@
 package com.example.meditations.repository
 
 import com.example.meditations.model.Meditation
+import io.reactivex.Completable
 import io.reactivex.Observable
 
 interface MeditationRepository {
     fun all(): Observable<List<Meditation>>
+    fun create(text: String): Completable
 }
--- app/src/main/java/com/example/meditations/repository/MeditationRepositoryImpl.kt
+++ app/src/main/java/com/example/meditations/repository/MeditationRepositoryImpl.kt
@@ -1,20 +1,29 @@
 package com.example.meditations.repository
 
+import android.util.Log
 import com.example.meditations.Database
 import com.example.meditations.model.Meditation
 import com.squareup.sqldelight.runtime.rx.asObservable
 import com.squareup.sqldelight.runtime.rx.mapToList
+import io.reactivex.Completable
 import io.reactivex.Observable
 import io.reactivex.schedulers.Schedulers
 
 class MeditationRepositoryImpl(val db: Database): MeditationRepository {
+    private val queries = db.meditationQueries
 
     override fun all(): Observable<List<Meditation>> {
-        return db.meditationQueries
+        return queries
             .selectAll()
             .asObservable(Schedulers.io())
             .mapToList()
             .map { it.map { Meditation.from(it) }  }
+    }
+
+    override fun create(text: String): Completable {
+        return Completable.fromCallable {
+            queries.insertMeditation(text)
+        }
     }
 
 }
