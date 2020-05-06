96a871db1e2c5f0fcd10931e9256a3f7b680374b
frenchdonuts <jonathantan1991@gmail.com>
2020-05-05 19:44:58 -0700

    add_meditation: Implement processEvents() and states() in AddMeditationVM;

--- app/src/main/java/com/example/meditations/add_meditation/vm/VM.kt
+++ app/src/main/java/com/example/meditations/add_meditation/vm/VM.kt
@@ -4,4 +4,8 @@ import androidx.lifecycle.ViewModel
 import com.example.meditations.add_meditation.ui.UI
 
 abstract class VM: com.example.meditations.base.VM<UI.Event, UI.State>, ViewModel() {
+    sealed class Msg {
+        object NoOp : Msg()
+    }
+    data class State(val uiState: UI.State = UI.State())
 }
